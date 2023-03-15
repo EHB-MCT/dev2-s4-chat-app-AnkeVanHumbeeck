f87f4c05d0425414a9fb6eda756c361c8506f25f
Anke Van Humbeeck <anke.van.humbeeck@student.ehb.be>
2023-03-15 12:26:05 +0100

    einde WC

--- js/index.js
+++ js/index.js
@@ -1,16 +1,34 @@
 "use strict";
 
 const chat = {
-    author: "yourName",
-    init() {
-    },
-    sendMessage() {
-    },
-    fetchMessages() {
-    },
-    renderMessage(message) {
-    }
+	author: "Anke",
+	init() {
+		this.fetchMessages();
+	},
+	sendMessage() {},
+	fetchMessages() {
+		fetch("https://dev2chat.onrender.com/messages")
+			.then(function (data) {
+				return data.json();
+			})
+			.then(function (jsonMessages) {
+				console.log(jsonMessages);
+				jsonMessages.forEach((msg) => {
+					const message = `<div class="messageItem">
+                    <div class="header">
+                        <span class="author">${msg.author}</span>
+                        <span class="time">00:00</span>
+                    </div>
+                        <p>
+                            ${msg.message}
+                        </p>
+                    </div>`;
 
-}
+					document.querySelector("#messageContainer").insertAdjacentHTML("beforeend", message);
+				});
+			});
+	},
+	renderMessage(message) {},
+};
 
 chat.init();
